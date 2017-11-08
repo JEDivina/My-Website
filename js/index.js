0f03f139a82ae809c67947ba9d37a2dc97305b1b
JEDivina <jeddivina@gmail.com>
2017-11-08 15:10:54 -0500

    made some small changes, edited About Me section

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 $(document).ready(function (){
 
-   var scroll_start = 0;
-   var startchange = $('#skills');
-   var offset = startchange.offset();
+   // var scroll_start = 0;
+   // var startchange = $('#skills');
+   // var offset = startchange.offset();
 
   $(".nav li").click(function(){
     $(".nav li").removeClass("active");
     $(this).addClass("active");
   });
 
-  if (startchange.length){
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', '#020731');
-        }
-       // } else {
-       //    $('.navbar-default').css('background-color', 'transparent');
-       // }
-   });
-  }
+  // if (startchange.length){
+  //  $(document).scroll(function() { 
+  //     scroll_start = $(this).scrollTop();
+  //     if(scroll_start > offset.top) {
+  //         $(".navbar-default").css('background-color', '#020731');
+  //       }
+  //      // } else {
+  //      //    $('.navbar-default').css('background-color', 'transparent');
+  //      // }
+  //  });
+  // }
 
 });
 
